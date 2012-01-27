2e4d7f9a908af2acc8cf11cc5815cefe906b91da
gangban.lau <gangban.lau@cebb60da-ff01-11dd-a320-db7d8e7a638f>
2012-01-27 03:20:10 +0000

    prevent including internal header files

--- sdk/include/snd.h
+++ sdk/include/snd.h
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2009 Gang Liu <gangban.lau@gmail.com>
+ * Copyright (C) 2009-2012 Gang Liu <gangban.lau@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,6 +15,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
  */
+#ifndef __OPENMEDIAENGINE__
+#error only libopenmediaengine should #include this header
+#endif
 
 #ifndef _SND_H_
 #define _SND_H_
